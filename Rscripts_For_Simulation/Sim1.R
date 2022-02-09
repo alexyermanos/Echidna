d16887c34c3dd03268dfedb9d394369286c46bd2
Jiami Han <67296294+hjm5545@users.noreply.github.com>
2022-02-09 13:25:12 +0100

    Update Sim1.R
    update package name

--- Rscripts_For_Simulation/Sim1.R
+++ Rscripts_For_Simulation/Sim1.R
@@ -1,7 +1,7 @@
 
 mus_b_h[[1]]<-mus_b_h[[1]][1:50,]
 mus_b_l[[1]]<-mus_b_l[[1]][1:30,]
-even<-scImmuneSimulation::simulate_repertoire(initial.size.of.repertoire = 5000,
+even<-simulate_repertoire(initial.size.of.repertoire = 5000,
                                               duration.of.evolution = 0,
                                               vdj.branch.prob = 0,
                                               cell.division.prob = c(0,0),
